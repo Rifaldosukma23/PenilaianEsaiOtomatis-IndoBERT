df783f34d8fa49ea70f7cfb9a5c78db3d91e0d25
Muhamad Taopik <mr.94t3z@gmail.com>
2023-07-05 11:43:00 +0700

    fixing code

--- app.py
+++ app.py
@@ -12,16 +12,20 @@ app.config["SECRET_KEY"] = 'rifaldo sukma s.'
 @app.route("/", methods=["POST", "GET"])
 @app.route("/dashboard", methods=["POST", "GET"])
 def index():
-    score = ""
-    if request.method == "POST":
-        question = request.form["question"]
-        kunci_jawaban = request.form["answer_key"]
-        jawaban_siswa = request.form.getlist("answer_students")
+    if request.method == 'POST':
+        question = request.form['question']
+        kunci_jawaban = request.form['kunci_jawaban']
+        jawaban_siswa_list = request.form.getlist('jawaban_siswa')
 
-        score = calculate_score(question, kunci_jawaban, jawaban_siswa)
+        results = []
+        for jawaban_siswa in jawaban_siswa_list:
+            result = calculate_score(question, kunci_jawaban, jawaban_siswa)
+            results.append(result)
 
-    return render_template("/index.html", score=score)
+        return render_template('result.html', scores=results, jawaban_siswa_list=jawaban_siswa_list, question=question, kunci_jawaban=kunci_jawaban, enumerate=enumerate)
+
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(host=os.getenv("API_HOST"), port=os.getenv("API_PORT"), debug=True)
+     app.run(host='localhost', port=2023, debug=True)
